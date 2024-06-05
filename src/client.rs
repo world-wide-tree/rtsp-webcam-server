ae0e1ebc3b01ba9666b8b1b65d0f20bdaae45162
Ali-0211-TR <xgamexarkon@gmail.com>
2024-06-06 01:29:10 +0500

    Working...

--- src/client.rs
+++ src/client.rs
@@ -1,14 +1,20 @@
 use opencv::highgui;
 use opencv::videoio;
 use opencv::prelude::*;
+use opencv::imgcodecs;
 
-fn main(){
+fn main() {
     let mut cam = videoio::VideoCapture::from_file_def("http://0.0.0.0:8787/").expect("Failed to get video capture");
     let mut frame = Mat::default();
     loop {
-        cam.read(&mut frame).unwrap();
+        if !cam.read(&mut frame).unwrap() || frame.size().unwrap().width <= 0 {
+            eprintln!("Failed to read frame");
+            break;
+        }
+
+        // let img = imgcodecs::imdecode(&frame, imgcodecs::IMREAD_COLOR).unwrap();
         highgui::imshow("ClientWindow", &frame).unwrap();
-        if ('q' as i32) == highgui::wait_key(1).unwrap(){
+        if ('q' as i32) == highgui::wait_key(35).unwrap() {
             break;
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,56 @@
 use opencv::{
-    core::{Mat, Vector}, imgcodecs, prelude::*, videoio,
+    core::{Mat, Vector}, highgui, imgcodecs, prelude::*, videoio
 };
 
 use std::net::TcpListener;
 use std::io::Write;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8787").unwrap();
     println!("Server listening on port 8787");
 
     let mut cam = videoio::VideoCapture::from_file_def("rtsp://admin:123qazwsx@192.168.1.64:554/Streaming/channels/102").expect("Failed to get video capture");
-    let mut frame = Mat::default();
-    let mut buf = Vector::new();
     
+    let mut frame = Mat::default();
+    if !cam.is_opened().expect("Capture not opened!"){
+        return;
+    }
+    // println!("Start streaming!");
+    // loop {
+    //     if !cam.read(&mut frame).unwrap() || frame.size().unwrap().width <= 0 {
+    //         eprintln!("Failed to read frame");
+    //         break;
+    //     }
+
+    //     // let img = imgcodecs::imdecode(&frame, imgcodecs::IMREAD_COLOR).unwrap();
+    //     highgui::imshow("ClientWindow", &frame).unwrap();
+    //     if ('q' as i32) == highgui::wait_key(1).unwrap() {
+    //         break;
+    //     }
+    // }
+    // println!("Error streaming!");
     for stream in listener.incoming() {
+        println!("connected");
+        let mut buf = Vector::new();
         let mut stream = stream.expect("Failed to accept connection");
         
         let response = format!(
             "HTTP/1.1 200 OK\r\nContent-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n"
         );
-
-        stream.write_all(response.as_bytes()).unwrap();
+        
+        if let Err(e) = stream.write_all(response.as_bytes()) {
+            eprintln!("Failed to write response: {}", e);
+            continue;
+        }
         
         loop {
-            cam.read(&mut frame).expect("Failed to capture frame");
+            let mut frame = Mat::default();
+            if !cam.read(&mut frame).expect("Failed to capture frame") || frame.size().unwrap().width <= 0 {
+                //eprintln!("Failed to read frame from camera, frame: {:#?}", frame);
+                //break;
+            }
             buf.clear();
             let _ = imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new());
 
@@ -32,40 +59,23 @@ fn main() {
                 buf.len()
             );
 
-            stream.write_all(image_data.as_bytes()).unwrap();
-            stream.write_all(buf.as_slice()).unwrap();
-            stream.write_all(b"\r\n").unwrap();
-            stream.flush().unwrap();
+            if let Err(e) = stream.write_all(image_data.as_bytes()) {
+                eprintln!("Failed to write image data: {}", e);
+                break;
+            }
+            if let Err(e) = stream.write_all(buf.as_slice()) {
+                eprintln!("Failed to write image buffer: {}", e);
+                break;
+            }
+            if let Err(e) = stream.write_all(b"\r\n") {
+                eprintln!("Failed to write image end: {}", e);
+                break;
+            }
+            if let Err(e) = stream.flush() {
+                eprintln!("Failed to flush stream: {}", e);
+                break;
+            }
+            thread::sleep(Duration::from_millis(30)); // добавьте небольшую задержку для предотвращения перегрузки
         }
     }
-
-    // loop {
-    //     let (mut stream, _) = listener.accept().expect("Failed to accept connection");
-        
-    //     cam.read(&mut frame).expect("Failed to capture frame");
-    //     buf.clear();
-    //     let _ = imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new());
-
-    //     let response = format!(
-    //         "HTTP/1.1 200 OK\r\nContent-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n"
-    //     );
-
-    //     stream.write_all(response.as_bytes()).unwrap();
-        
-    //     loop {
-    //         cam.read(&mut frame).expect("Failed to capture frame");
-    //         buf.clear();
-    //         let _ = imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new());
-
-    //         let image_data = format!(
-    //             "--frame\r\nContent-Type: image/jpeg\r\nContent-Length: {}\r\n\r\n",
-    //             buf.len()
-    //         );
-
-    //         stream.write_all(image_data.as_bytes()).unwrap();
-    //         stream.write_all(buf.as_slice()).unwrap();
-    //         stream.write_all(b"\r\n").unwrap();
-    //         stream.flush().unwrap();
-    //     }
-    // }
 }
